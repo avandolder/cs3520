1fe3cbd0f287c740b5b1bf5586d7e514221cd5f3
Adam Vandolder <adam.vandolder@gmail.com>
2020-01-13 19:01:17 -0500

    Fixed Bresenham's for all slopes

--- draw_line/draw_line.cpp
+++ draw_line/draw_line.cpp
@@ -1,9 +1,15 @@
+#include <algorithm>
 #include <cmath>
+#include <cstdlib>
 #include <unistd.h>
 #include <utility>
 
 #include <GL/freeglut.h>
 #include <GL/gl.h>
+
+enum class Key: unsigned char {
+  Escape = 27,
+};
 
 const int WIDTH = 500, HEIGHT = 500;
 int mx = WIDTH / 2, my = HEIGHT / 2;
@@ -48,58 +54,59 @@ void dda_line(int x1, int y1, int x2, int y2) {
 void bresenham_line(int x1, int y1, int x2, int y2) {
   glBegin(GL_POINTS);
 
-  int x = x1, y = y1;
   const int dx = x2 - x1;
   const int dy = y2 - y1;
+  const int dx2 = std::abs(2 * dx);
+  const int dy2 = std::abs(2 * dy);
+  const double m = (double)dy / (double)dx;
+  const int step = (m > 0) ? 1 : -1;
 
   if (dx == 0) {
     // Vertical line.
-    for (; y <= y2; ++y) {
-      glVertex2f(x, y);
+    for (int y = std::min(y1, y2); y <= std::max(y1, y2); ++y) {
+      glVertex2f(x1, y);
     }
   } else if (dy == 0) {
     // Horizontal line.
-    for (; x <= x2; ++x) {
-      glVertex2f(x, y);
+    for (int x = std::min(x1, x2); x <= std::max(x1, x2); ++x) {
+      glVertex2f(x, y1);
     }
-  } else if (dx == dy) {
-    // Slope is +/- 1.
-    const int stepx = (dx > 0) ? 1 : -1;
-    const int stepy = (dy > 0) ? 1 : -1;
-    for (; x <= x2; x += stepx, y += stepy) {
+  } else if (std::abs(m) < 1) {
+    // Slope is between -1 and 1, so step by x.
+    const int diff = dy2 - dx2;
+    int p = dy2 - dx;
+
+    if (x1 > x2) {
+      std::swap(x1, x2);
+      std::swap(y1, y2);
+    }
+
+    for (int x = x1, y = y1; x <= x2; ++x) {
       glVertex2f(x, y);
+      if (p < 0) {
+        p += dy2;
+      } else {
+        y += step;
+        p += diff;
+      }
     }
   } else {
-    const int dx2 = 2 * dx;
-    const int dy2 = 2 * dy;
-    const int step = (dx < 0) == (dy < 0) ? 1 : -1;
+    // Slope is outside of -1 and 1, so step by y.
+    const int diff = dx2 - dy2;
+    int p = dx2 - dy;
 
-    if (dy < dx) {
-      // If -1 < slope < 1.
-      int diff = dy2 - dx2;
-      int p = dy2 - dx;
+    if (y1 > y2) {
+      std::swap(x1, x2);
+      std::swap(y1, y2);
+    }
 
-      for (; x <= x2; ++x) {
-        glVertex2f(x, y);
-        if (p < 0) {
-          p += dy2;
-        } else {
-          y += step;
-          p += diff;
-        }
-      }
-    } else {
-      int diff = dx2 - dy2;
-      int p = dx2 - dy;
-
-      for (; y <= y2; ++y) {
-        glVertex2f(x, y);
-        if (p < 0) {
-          p += dx2;
-        } else {
-          x += step;
-          p += diff;
-        }
+    for (int x = x1, y = y1; y <= y2; ++y) {
+      glVertex2f(x, y);
+      if (p < 0) {
+        p += dx2;
+      } else {
+        x += step;
+        p += diff;
       }
     }
   }
@@ -112,23 +119,33 @@ void handle_mouse_movement(int x, int y) {
   my = HEIGHT - y;
 }
 
+void handle_key(unsigned char key, int x, int y) {
+  switch (static_cast<Key>(key)) {
+    case Key::Escape:
+      std::exit(0);
+  }
+}
+
 void show_screen() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   // Draw various lines using Bresenham's algorithm.
   glColor3f(0.0, 0.0, 1.0);
   bresenham_line(0, 0, 250, 125);
+  bresenham_line(0, 125, 250, 0);
   glColor3f(1.0, 0, 0);
   bresenham_line(0, 250, 125, 500);
+  bresenham_line(0, 500, 125, 250);
   glColor3f(0, 1, 0);
-  bresenham_line(0, 250, 500, 250);
+  bresenham_line(500, 250, 0, 250);
   bresenham_line(250, 0, 250, 500);
   glColor3f(1, 1, 0);
   bresenham_line(250, 250, 500, 500);
+  bresenham_line(500, 250, 250, 500);
 
   // Draw a line from the center of the window to the mouse using the DDA algorithm.
   glColor3f(1.0, 1.0, 1.0);
-  dda_line(WIDTH / 2, HEIGHT / 2, mx, my);
+  bresenham_line(WIDTH / 2, HEIGHT / 2, mx, my);
 
   glutSwapBuffers();
 
@@ -146,7 +163,7 @@ int main (int argc, char **argv) {
 
   // Initialize OpenGL.
   glClearColor(0.0, 0.0, 0.0, 0.0);
-  glPointSize(2.0);
+  glPointSize(1.0);
   glMatrixMode(GL_PROJECTION);
   glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
 
@@ -154,6 +171,7 @@ int main (int argc, char **argv) {
   glutDisplayFunc(show_screen);
   glutMotionFunc(handle_mouse_movement);
   glutPassiveMotionFunc(handle_mouse_movement);
+  glutKeyboardFunc(handle_key);
 
   glutMainLoop();
   return 0;
