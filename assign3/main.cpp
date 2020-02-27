efeb6cc385282b28e690907a17ba200b0fe586c7
Adam Vandolder <adam.vandolder@gmail.com>
2020-02-27 14:19:00 -0500

    Switch to GLdouble, use x/y/z instead of spherical coords

--- assign3/main.cpp
+++ assign3/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
@@ -10,50 +9,72 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 
+// Suppress unused parameter warnings from gcc.
+#ifdef __GNUC__
+#  define UNUSED __attribute__((unused))
+#else
+#  define UNUSED
+#endif
+
 struct Vertex {
-  GLfloat coords[3] = {0.0, 0.0, 0.0};
+  GLdouble coords[3] = {0.0, 0.0, 0.0};
 
   Vertex() {}
-  Vertex(GLfloat x, GLfloat y, GLfloat z) : coords{x, y, z} {}
+  Vertex(GLdouble x, GLdouble y, GLdouble z) : coords{x, y, z} {}
 
-  inline GLfloat& operator[](int idx) { return coords[idx]; }
+  inline GLdouble& operator[](int idx) { return coords[idx]; }
 };
 
 struct Polygon {
-  Vertex origin {0.0, 0.0, 0.0};
   std::vector<Vertex> vertices;
-  GLfloat color[3];
+  GLdouble color[3];
 
   inline Vertex& operator[](int idx) { return vertices[idx]; }
 };
 
 struct Object {
-  Vertex origin {0.0, 0.0, 0.0};
+  Vertex origin;
+  Vertex rotation_axis;
+  GLdouble rotation_angle = 0.0;
+
   std::vector<Polygon> faces;
 
   static Object read(const std::string& path) {
     std::ifstream file(path);
     Object object;
 
-    std::string type;
+    std::string line;
     Polygon face;
-    while (file >> type) {
+    while (std::getline(file, line)) {
+      if (line[0] == '#') {
+        // Skip comment lines.
+        continue;
+      }
+
+      std::istringstream str(line);
+      std::string type;
+      str >> type;
       if (type == "color") {
-        file >> face.color[0];
-        file >> face.color[1];
-        file >> face.color[2];
+        str >> face.color[0];
+        str >> face.color[1];
+        str >> face.color[2];
       } else if (type == "face") {
         face = Polygon();
       } else if (type == "vertex") {
         Vertex v;
-        file >> v[0];
-        file >> v[1];
-        file >> v[2];
+        str >> v[0];
+        str >> v[1];
+        str >> v[2];
         face.vertices.push_back(v);
       } else if (type == "origin") {
-        file >> object.origin[0];
-        file >> object.origin[1];
-        file >> object.origin[2];
+        str >> object.origin[0];
+        str >> object.origin[1];
+        str >> object.origin[2];
+      } else if (type == "rotation") {
+        str >> object.rotation_angle;
+        str >> object.rotation_axis[0];
+        str >> object.rotation_axis[1];
+        str >> object.rotation_axis[2];
       } else if (type == "end") {
         object.faces.push_back(face);
       }
@@ -65,36 +86,36 @@ struct Object {
   void render() {
     for (auto& face : faces) {
       glPushMatrix();
-      glColor3f(face.color[0], face.color[1], face.color[2]);
-      glTranslatef(origin[0], origin[1], origin[2]);
+      glColor3d(face.color[0], face.color[1], face.color[2]);
+      glTranslated(origin[0], origin[1], origin[2]);
+      glRotated(rotation_angle, rotation_axis[0], rotation_axis[1], rotation_axis[2]);
+
       glBegin(GL_POLYGON);
       for (auto& v : face.vertices) {
-        glVertex3f(v[0], v[1], v[2]);
+        glVertex3d(v[0], v[1], v[2]);
       }
       glEnd();
+      
       glPopMatrix();
     }
   }
-};
-std::vector<Object> objects;
+} house = Object::read("house.obj");
 
-GLfloat width = 500.0, height = 500.0;
+GLdouble width = 500.0, height = 500.0;
 int fps = 60;
 unsigned int frame_time = 1000 / fps;
 
-Vertex eye;
-GLfloat eye_radius = 8.0;
-GLfloat eye_theta = M_PI / 2.0;
-GLfloat eye_phi = 0.0;
+Vertex eye {0.0, 0.0, 8.0};
+GLdouble horz_rot, vert_rot;
 
 bool key_down[256] = {false};
 
-void tick(int v) {
+void tick(UNUSED int v) {
   // Run tick again, at approximately 60 FPS.
   glutTimerFunc(frame_time, tick, 0);
 
   std::stringstream title;
-  title << "Assignment 2 Radius:" << eye_radius << " Theta:" << eye_theta << " Phi:" << eye_phi;
+  title << "Assignment 2 X:" << eye[0] << " Y:" << eye[1] << " Z:" << eye[2];
   glutSetWindowTitle(title.str().c_str());
 
   // Calculated time elapsed since the last frame.
@@ -104,16 +125,16 @@ void tick(int v) {
 
   // Handle key presses.
   if (key_down['d']) {
-    eye_phi = std::fmod(eye_phi + M_PI*dt/2, M_PI * 2);
+    horz_rot = std::fmod(horz_rot + 360*dt/2, 360);
   }
   if (key_down['a']) {
-    eye_phi = std::fmod(eye_phi - M_PI*dt/2, M_PI * 2);
+    horz_rot = std::fmod(horz_rot - 360*dt/2, 360);
   }
   if (key_down['w']) {
-    eye_theta = std::max(eye_theta - M_PI*dt/2, 0.001);
+    vert_rot = std::fmod(vert_rot + 360*dt/2, 360);
   }
   if (key_down['s']) {
-    eye_theta = std::min(eye_theta + M_PI*dt/2, M_PI - 0.01);
+    vert_rot = std::fmod(vert_rot - 360*dt/2, 360);
   }
 
   glutPostRedisplay();
@@ -129,17 +150,15 @@ void render() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  eye[0] = eye_radius * std::sin(eye_theta) * std::cos(eye_phi);
-  eye[2] = eye_radius * std::sin(eye_theta) * std::sin(eye_phi);
-  eye[1] = eye_radius * std::cos(eye_theta);
   gluLookAt(eye[0], eye[1], eye[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
   glClearColor(0.0, 0.0, 0.0, 0.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  for (auto& obj : objects) {
-    obj.render();
-  }
+  glRotated(horz_rot, 0.0, 1.0, 0.0);
+  glRotated(vert_rot, 1.0, 0.0, 0.0);
+
+  house.render();
 
   glutSwapBuffers();
 }
@@ -151,16 +170,14 @@ void reshape(int w, int h) {
 
 void handle_mouse(int button, int state, int x, int y) {
   if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-    auto xx = eye[0] + x * 2 / width - 1.0;
-    auto yy = eye[1] + 2 * (height - y) / height - 1.0;
-    std::cout << x * 2 / width - 1.0 << " " << 2 * (height - y) / height << std::endl;
-
-    eye_phi = std::atan2(yy, xx);
-    eye_theta = std::atan2(std::sqrt(xx*xx + yy*yy), eye_radius);
+    GLdouble dx = (x * 2.0) / width - 1.0;
+    GLdouble dy = ((height - y) * 2.0) / height - 1.0;
+    eye[0] += dx;
+    eye[1] += dy;
   }
 }
 
-void handle_key(unsigned char key, int x, int y) {
+void handle_key(unsigned char key, UNUSED int x, UNUSED int y) {
   if (key == 27) {
     // Quit on escape key.
     std::exit(0);
@@ -169,17 +186,17 @@ void handle_key(unsigned char key, int x, int y) {
   key_down[key] = true;
 }
 
-void handle_keyup(unsigned char key, int x, int y) {
+void handle_keyup(unsigned char key, UNUSED int x, UNUSED int y) {
   key_down[key] = false;
 }
 
 void menu(int option) {
   switch (option) {
     case 1:
-      eye_radius += 1.0;
+      eye[2] += 1.0;
       break;
     case 2:
-      eye_radius -= 1.0;
+      eye[2] -= 1.0;
       break;
     case 3:
       std::exit(0);
@@ -203,14 +220,12 @@ int main (int argc, char **argv) {
   glutKeyboardUpFunc(handle_keyup);
   glutTimerFunc(1, tick, 0);
 
+  // Create the context menu.
   glutCreateMenu(menu);
-  glutAddMenuEntry("Increment Eye Distance", 1);
-  glutAddMenuEntry("Decrement Eye Distance", 2);
+  glutAddMenuEntry("Increment Eye Z", 1);
+  glutAddMenuEntry("Decrement Eye Z", 2);
   glutAddMenuEntry("Quit", 3);
   glutAttachMenu(GLUT_RIGHT_BUTTON);
-
-  // Load the object.
-  objects.push_back(Object::read("house.obj"));
 
   glutMainLoop();
 }
